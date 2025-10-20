8b8aba0b4d346457df6e84e86b5e435a88c481bc
Hector5472 <hectormartinn2005@gmail.com>
2025-10-20 09:49:18 +0100

    3º Commit: Código desplazado a Ficha.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
-import { useState } from 'react'
-import snakeImg from './assets/snakeImg.jpg'
 import './App.css'
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Appgrid from './Appgrid';
 
+import Ficha from './components/Ficha'
 import FormRegistro from './components/FormRegistro'
 
+
 function App() {
-  const [petCount, setCount] = useState(0)
+  
 
   return (
     <>
-
-      <Stack
-        direction="column"
-        spacing={{xs: 1, sm: 2, md: 4}}
-        sx={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant='h1'>Sssoy una fokin serpiente 7~7</Typography>
-
-        <a>
-          <Avatar sx={{ width: 150, height: 150 }} src={snakeImg} className="avatar" alt="imagen" />
-        </a>
-
-        <br></br>
-
-        <Button variant='contained' size='medium' color='secondary' sx={{ color: 'yellow' }} onClick={() => setCount((count) => count + 1)}>
-          me has hecho {petCount} rascaditas
-        </Button>
-        <Appgrid></Appgrid>
-        
-        <FormRegistro/>
-      </Stack>
+<Ficha/>
+<FormRegistro/>
     </>
 
   )
